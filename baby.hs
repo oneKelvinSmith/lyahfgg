2e44da335bd975621f579b140c7c736eabe1d6b6
Kelvin Smith <oneKelvinSmith@gmail.com>
2017-03-24 22:39:49 +0000

    Finish chapter 1

--- baby.hs
+++ baby.hs
@@ -18,3 +18,20 @@ length' items = sum [1 | _ <- items]
 
 removeNonUppercase string =
   [character | character <- string, character `elem` ['A'..'Z']]
+
+triples = [(a,b,c) | c <- [1..10], a <- [1..10], b <- [1..10]]
+
+rightTriangles =
+  [(a,b,c) |
+   c <- [1..10],
+   a <- [1..c],
+   b <- [1..a],
+   a^2 + b^2 == c^2]
+
+rightTriangles' =
+  [(a,b,c) |
+   c <- [1..10],
+   a <- [1..c],
+   b <- [1..a],
+   a^2 + b^2 == c^2,
+   a + b + c == 24]
